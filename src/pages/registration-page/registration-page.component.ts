6c394ea7f78992b24cac6c1a39e13ac8610970f1
igorLinden <>
2026-01-29 22:38:50 +0300

    fix: add standalone parameter

--- src/pages/registration-page/registration-page.component.ts
+++ src/pages/registration-page/registration-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration-page',
+  standalone: true,
   imports: [],
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.scss',
