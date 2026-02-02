84666618a0cecf33558b5d68cda2926a7bf0a7ba
I.Lipnitskij <I.Lipnitskij@beloil.by>
2026-02-02 13:16:34 +0300

    build: add routes + tailwind init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from '../pages/home-page/home-page.component';
+import { LoginPageComponent } from '../pages/login-page/login-page.component.js'
+import { RegistrationPageComponent } from '../pages/registration-page/registration-page.component';
+import { NotFoundPageComponent } from '../pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: HomePageComponent,
+    title: 'ToDoo',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    title: 'Login',
+  },
+  {
+    path: 'registration',
+    component: RegistrationPageComponent,
+    title: 'Registration',
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+    title: '404 - Not Found',
+  },
 ];
--- src/pages/login-page/login-page.component.ts
+++ src/pages/login-page/login-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-login-page',
+  selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Login data:', this.form.value);
+      // Здесь будет dispatch или вызов сервиса
+    }
+  }
 }
