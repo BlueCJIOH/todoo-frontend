56cace74e935e3163dd1f1f900b8db3a475267d2
I.Lipnitskij <I.Lipnitskij@beloil.by>
2026-02-04 10:13:00 +0300

    feat(auth): add toggle password visability + change form styles

--- src/pages/login-page/login-page.component.ts
+++ src/pages/login-page/login-page.component.ts
@@ -3,26 +3,36 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  public hidePassword: boolean = true;
+
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
 
-  onSubmit() {
+  protected togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  public onSubmit() {
     if (this.form.valid) {
       console.log('Login data:', this.form.value);
       // Здесь будет dispatch или вызов сервиса
